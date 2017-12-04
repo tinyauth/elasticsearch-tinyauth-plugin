c84b7e17365dea194fcde5c783638b7d14d32925
John Carr <john.carr@unrouted.co.uk>
2017-12-04 10:36:34 +0000

    Add missing file

---
+++ src/main/java/io/tinyauth/elasticsearch/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2017 tinyauth.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.tinyauth.elasticsearch;
+
+public class Constants {
+  public static final String PREFIX = "_tinyauth_";
+
+  public static final String HEADERS = PREFIX + "headers";
+  public static final String SOURCE_IP = PREFIX + "source_ip";
+}
